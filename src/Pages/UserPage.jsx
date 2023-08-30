f12dfd539cfbd740fe079eb274261f9500aa3e71
Aakash <aakashit13@gmail.com>
2023-08-30 15:53:07 +0530

    coursesCardsRender |

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardCourse from "../Components/CardCourse";
+
 const UserPage = () => {
-  return (
-    <div>
-      <CardCourse />
-    </div>
-  );
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/users/courses")
+      .then(res => res.json())
+      .then(data => setCourses(data));
+  }, []);
+  const renderCards =
+    courses.length !== 0
+      ? courses.map(course => {
+          return (
+            <div key={course._id}>
+              <CardCourse
+                courseName={course.title}
+                courseImageLink={course.imageLink}
+                courseDescription={course.description}
+                coursePrice={course.price}
+                courseAuthorName={course.title}
+              />
+            </div>
+          );
+        })
+      : false;
+  // TODO Modify Styling
+  return <div className="flex">{renderCards}</div>;
 };
 
 export default UserPage;
